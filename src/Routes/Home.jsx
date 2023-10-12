9789932fe0fd0f29887593db87b9a8f5ca75c256
numanrahi <arahi2994@gmail.com>
2023-10-12 23:43:10 +0600

    category removed

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../pages/Home/Navbar/Navbar";
 import Banner from "../pages/Home/Banner/Banner";
-import Category from "../pages/Home/Category/Category";
 import Product from "../pages/Home/Product/Product";
 import Gallery from "../pages/Home/Gallery/Gallery";
 
